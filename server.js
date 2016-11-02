6c65669b7fd22685f36acaf7c6a0433958f0003c
James Echmalian <ech@ech.net>
2016-11-02 17:28:04 -0400

    some refactoring

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 /* server.js */
 
-var express = require("express");
-var server = express();
-server.set("port", process.env.PORT || 4567);
-server.use(express.static("www"));
-server.listen(server.get("port"), function () {
-  console.log("Listening on port", server.get("port"));
-});
+var CONFIG = {
+  server: {
+    port: 4567,
+    baseDir: "."
+  },
+  javascript: {
+    jqueryVersion: "1.7.2",
+  }
+}
+
+var CONFIG_JS = '' +
+  'var require = { \n' +
+  '  baseUrl: "/js", \n' +
+  '  paths: { \n' +
+  '    "jquery": "jquery-' + CONFIG.javascript.jqueryVersion + '" \n' +
+  '  }, \n' +
+  '  map: { \n' +
+  '    "*": { "jquery": "jquery-private" }, \n' +
+  '    "jquery-private": { "jquery": "jquery" } \n' +
+  '  }, \n' +
+  '  deps: [ "app" ], \n' +
+  '  callback: function(app) { app(); } \n' +
+  '}\n';
+
+function newServer() {
+  var express = require("express");
+  var server = express();
+  server.use(express.static(CONFIG.server.baseDir));
+  return server;
+}
+
+function handleConfigJs(req, res) {
+  res.set("Content-Type", "application/javascript");
+  res.send(CONFIG_JS);
+}
+
+(function() {
+  var server = newServer();
+  var port = process.env.PORT || CONFIG.server.port;
+
+  server.set("port", port);
+
+  server.get("/config.js", function(req, res) {
+    res.set("Content-Type", "application/javascript");
+    res.send(CONFIG_JS);
+  });
+
+  server.listen(port, function () {
+    console.log("Listening on port", port);
+  });
+})();
